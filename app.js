18b9600171c1fead39d8679eddcdb16e770e4175
liyababu <50897099+liyababu@users.noreply.github.com>
2020-05-20 12:38:00 -0600

    Ignore favicon

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var hbs = require('hbs');
 var session = require('express-session');
 var http = require('http');
 var debug = require('debug');
 var index = require('./routes/index');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,6 +40,14 @@ app.use(function(req, res, next) {
 app.use('/', index);
 var http = require('http');
 
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl === '/favicon.ico') {
+    res.status(204).json({nope: true});
+  } else {
+    next();
+  }
+}
+app.use(ignoreFavicon);
 /**
  * Get port from environment and store in Express.
  */

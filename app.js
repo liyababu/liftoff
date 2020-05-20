70ed25e8755f5f9897b9e2695c13b7b433518486
liyababu <50897099+liyababu@users.noreply.github.com>
2020-05-20 10:29:41 -0600

    Working on server connection

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var hbs = require('hbs');
 var session = require('express-session');
-
+var http = require('http');
+var debug = require('debug');
 var index = require('./routes/index');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,27 +32,26 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', index);
+var http = require('http');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+/**
+ * Get port from environment and store in Express.
+ */
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+var port = (process.env.PORT || '3000');
+app.set('port', port);
 
-// use port 3000 unless there exists a preconfigured port
-var port = process.env.port || 3000;
+/**
+ * Create HTTP server.
+ */
 
-app.listen(port);
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+
 
 module.exports = app;
